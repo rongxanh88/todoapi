185dd0010b584a665e14d293c64c7f4e29e1dd8a
Bao Nguyen <rongxanh88@gmail.com>
2023-08-16 19:28:01 -0600

    Add additional types

--- types.go
+++ types.go
@@ -1,7 +1,17 @@
 package main
 
+import "time"
+
 type Todo struct {
-	ID          int    `json:"id"`
+	ID          int       `json:"id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Completed   bool      `json:"completed"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
+
+type CreateTodoRequest struct {
 	Title       string `json:"title"`
 	Description string `json:"description"`
 	Completed   bool   `json:"completed"`
@@ -12,5 +22,7 @@ func NewTodo(title string, description string, completed bool) *Todo {
 		Title:       title,
 		Description: description,
 		Completed:   completed,
+		CreatedAt:   time.Now().UTC(),
+		UpdatedAt:   time.Now().UTC(),
 	}
 }
